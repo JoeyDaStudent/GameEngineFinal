c2ad26ef2935f6e9b67e5b5ae7a35e55e1a5d4dd
JoeyTheSlayer <joseph.carrillo@ontariotechu.net>
2021-12-17 18:10:38 -0500

    GameEngineFinal

---
+++ LowFidelityDuckHunt/Assets/Scripts/Gun.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    float speed = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
